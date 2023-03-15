296e42425884219057d954b2e0a19ba4de5e40da
Nagib Mahfuz <nmfuad.bd@gmail.com>
2023-03-15 09:48:19 +0600

    time change

--- js/time.js
+++ js/time.js
@@ -1,25 +1,64 @@
-// const url = "https://worldtimeapi.org/api/timezone/asia/dhaka";
-// fetch(url)
-//     .then(res => res.json())
-//     .then(data => countryList(data))
+const url = "https://worldtimeapi.org/api/timezone/asia/dhaka";
+fetch(url)
+    .then(res => res.json())
+    .then(data => countryList(data))
 
 // function countryList(data) {
-//     const timeElement = document.getElementById('time');
-//     // updateTime(timeElement, data.datetime);
-//     setInterval(() => {
-//         const date = new Date(data.datetime);
-//         console.log(date);
-//         const hours = date.getHours();
-//         const minutes = date.getMinutes();
-//         const seconds = date.getSeconds();
-//         console.log(seconds);
-//         const ampm = hours >= 12 ? 'pm' : 'am';
-//         const hours12 = hours % 12 || 12;
-//         const formattedTime = `${hours12}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${ampm}`;
-//         timeElement.innerText = formattedTime;
-//     }, 100);
 // }
 
+    const timeElement = document.getElementById('time');
+    const date = new Date(data.datetime);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const ampm = hours >= 12 ? 'pm' : 'am';
+    const hours12 = hours % 12 || 12;
+    const formattedTime = `${hours12}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${ampm}`;
+    timeElement.innerText = formattedTime;
 
-let name = prompt("Please enter your name:");
+// ==================================================================
+// ---------------------- new function ------------------------------
+// ==================================================================
+// let miliSecInterval;
 
+// let miliSec = 1;
+let sec = 0;
+let min = 0;
+let hour = 0;
+
+ function countryList(data) {
+
+    function creatTime(timeId, time) {
+        let id = timeTwoDigit(time);
+        document.getElementById(timeId).innerText = `${id}`
+    }
+
+    setInterval(() => {
+
+        // creatTime('milisecond', miliSec);
+        creatTime('second', sec);
+        creatTime('min', min);
+        creatTime('hour', hour);
+
+        miliSec++
+        if (miliSec === 100) {
+            sec++
+            miliSec = 0;
+        }
+        if (sec === 60) {
+            min++
+            sec = 0;
+        }
+    }, 10)
+}
+
+
+
+function timeTwoDigit(time) {
+    let id = time.toString();
+    if (id.length === 1) {
+        id = '0' + time;
+    } else {
+        id = time;
+    } return id;
+}
